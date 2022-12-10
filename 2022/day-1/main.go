624d5d40aac879904e3d0ee7e82fdcf1a041728a
1Mill <erik.ekberg@outlook.com>
2022-12-10 14:25:56 -0800

    Sudocode

--- 2022/day-1/main.go
+++ 2022/day-1/main.go
@@ -2,8 +2,51 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
 
+type Calorie int
+type Elf []Calorie
+type Elves []Elf
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
-	fmt.Println("Hello world!")
+	file := "./example.txt"
+	// file := "./input.txt"
+
+	// * Import file as bytes
+	data, err := os.ReadFile(file)
+	check(err)
+
+	// * Turn bytes into string
+	contnet := string(data)
+
+	// * Split string by double new lines to get elf sub-strings
+	// * Split elf sub-strings by new lines
+	// * Convert lines to numbers
+	groups := strings.Split(contnet, "\n\n")
+	elves := make(Elves, len(groups))
+
+	// for _, group := range groups {
+	// for j, line := range strings.Split(group, "\n") {
+	// 	elf := make([]int, len())
+
+	// 	if line == "" {
+	// 		continue
+	// 	}
+
+	// 	num, err := strconv.Atoi(line)
+	// 	check(err)
+
+	// 	elves[i][j] = num
+	// }
+	// }
+
+	fmt.Println(elves)
 }
